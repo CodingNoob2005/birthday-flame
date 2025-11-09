517a52e3302f411f652f1d0ad3df83194b17cfb2
Vanshika Gupta <vgup0011@student.monash.edu>
2025-11-09 17:58:07 +1100

    message done

--- src/components/BirthdayCard.js
+++ src/components/BirthdayCard.js
@@ -12,6 +12,12 @@ export default function BirthdayCard({ onClose }) {
         <div className="card-inner">
           <h3>To Soham,</h3>
           <p>
+            Remember to always keep your spirit of lightness and joy. 
+            Ups and downs are part of life, but your positive outlook makes all the difference in how 
+            we live through it. May your chi shine through everything!
+            To the sunshine of our house, happy birthday, little one!
+          </p>
+          <p> 
             May your year be filled with laughter, creativity, and sweet memories.
             Keep shining bright!
           </p>
